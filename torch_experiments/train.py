e04fd3669428a408c8efa8097e5eb2cf27b839c3
KonstFed <konstantin_fedorov_03@mail.ru>
2023-12-17 18:18:04 +0000

    theory SGD experiment & plot

--- torch_experiments/train.py
+++ torch_experiments/train.py
@@ -5,15 +5,18 @@ from torch.optim import Optimizer
 
 
 def train_model(
-    dataloader: DataLoader, model, loss_fn, optimizer: Optimizer, device="cpu"
+    dataloader: DataLoader, model, loss_fn, optimizer: Optimizer, device="cpu", verbose=True
 ):
     device = torch.device(device)
     model.to(device)
-    bar = tqdm(total=len(dataloader))
+    if verbose:
+        bar = tqdm(total=len(dataloader))
     loss_data = []
+    acc_data = []
     model.train()
     
-
+    tp = 0
+    total = 0
     for i, (data, label) in enumerate(dataloader):
         optimizer.zero_grad()
         data = data.to(device)
@@ -21,21 +24,29 @@ def train_model(
         label = label.to(device)
 
         output = model(data)
+        
+        pred = output.argmax(dim=1)
+        tp += (pred == label).sum()
+        total += label.shape[0]
+        acc_data.append((tp/total).detach().cpu())
+        
         loss = loss_fn(output, label)
         loss.backward()
         optimizer.step()
 
         loss_data.append(loss.detach().cpu())
+        if verbose:
+            bar.update(1)
+            bar.set_postfix({"loss": loss.detach().cpu().numpy()})
+    return loss_data, acc_data
 
-        bar.update(1)
-        bar.set_postfix({"loss": loss.detach().cpu().numpy()})
-    return loss_data
 
 
-
-def test_model(dataloader: DataLoader, model, loss_fn, device="cpu"):
-    bar = tqdm(total=len(dataloader))
+def test_model(dataloader: DataLoader, model, loss_fn, device="cpu", verbose=True):
+    if verbose:
+        bar = tqdm(total=len(dataloader))
     loss_data = []
+    acc_data = []
     device = torch.device(device)
     model.to(device)
     model.eval()
@@ -54,9 +65,11 @@ def test_model(dataloader: DataLoader, model, loss_fn, device="cpu"):
             pred = output.argmax(dim=1)
             tp += (pred == label).sum()
             total += label.shape[0]
+            acc_data.append((tp/total).detach().cpu())
 
             loss_data.append(loss.detach().cpu())
-            bar.update(1)
-            bar.set_postfix({"loss": loss.detach().cpu().numpy(), "acc": {tp / total}})
+            if verbose:
+                bar.update(1)
+                bar.set_postfix({"loss": loss.detach().cpu().numpy(), "acc": {acc_data[-1]}})
 
-    return loss_data, tp / total
+    return loss_data, tp / total, acc_data
